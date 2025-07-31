c9d39610f24c27888686cbf0d4714f0473ce59d5
JoserthLuc26 <Joserthlucena@gmail.com>
2025-07-30 22:34:52 -0400

    Agregados primeros archivos

---
+++ html/index.php
@@ -0,0 +1,86 @@
+<?php require_once '../secure/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Cental - Car Rent Website Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="static/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="static/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="static/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="static/css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <?php include 'spinnerStart.php'; ?>
+
+    <?php include 'topbarStart.php'; ?>
+
+    <?php include 'navbar.php'; ?>
+
+    <?php include 'carousel.php'; ?>
+
+    <?php include 'featuresStart.php'; ?>
+
+    <?php include 'aboutStart.php'; ?>
+
+    <?php include 'factCouter.php'; ?>
+
+    <?php include 'servicesStart.php'; ?>
+
+    <?php include 'carCategoriesStart.php'; ?>
+
+    <?php include 'carStepsStart.php'; ?>
+    
+    <?php include 'blogStart.php'; ?>
+
+    <?php include 'bannerStart.php'; ?>
+
+    <?php include 'teamStart.php'; ?>
+
+    <?php include 'testimonialStart.php'; ?>
+
+    <?php include 'footerStart.php'; ?>
+
+    <?php include 'copyrightStart.php'; ?>
+
+    <?php include 'backToTop.php'; ?>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="static/lib/wow/wow.min.js"></script>
+    <script src="static/lib/easing/easing.min.js"></script>
+    <script src="static/lib/waypoints/waypoints.min.js"></script>
+    <script src="static/lib/counterup/counterup.min.js"></script>
+    <script src="static/lib/owlcarousel/owl.carousel.min.js"></script>
+
+
+    <!-- Template Javascript -->
+    <script src="static/js/main.js"></script>
+  </body>
+
+</html>
---
+++ html/navbar.php
@@ -0,0 +1,36 @@
+<!-- Navbar & Hero Start -->
+<div class="container-fluid nav-bar sticky-top px-0 px-lg-4 py-2 py-lg-0">
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <a href="" class="navbar-brand p-0">
+        <h1 class="display-6 text-primary"><i class="fas fa-car-alt me-3"></i></i>Cental</h1>
+        <!-- <img src="img/logo.png" alt="Logo"> -->
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="fa fa-bars"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav mx-auto py-0">
+          <a href="index.html" class="nav-item nav-link active">Home</a>
+          <a href="del/about.html" class="nav-item nav-link">About</a>
+          <a href="del/service.html" class="nav-item nav-link">Service</a>
+          <a href="del/blog.html" class="nav-item nav-link">Blog</a>
+
+          <div class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+            <div class="dropdown-menu m-0">
+              <a href="del/feature.html" class="dropdown-item">Our Feature</a>
+              <a href="del/cars.html" class="dropdown-item">Our Cars</a>
+              <a href="del/team.html" class="dropdown-item">Our Team</a>
+              <a href="del/testimonial.html" class="dropdown-item">Testimonial</a>
+              <a href="del/404.html" class="dropdown-item">404 Page</a>
+            </div>
+          </div>
+          <a href="del/contact.html" class="nav-item nav-link">Contact</a>
+        </div>
+        <a href="#" class="btn btn-primary rounded-pill py-2 px-4">Get Started</a>
+      </div>
+    </nav>
+  </div>
+</div>
+<!-- Navbar & Hero End -->
